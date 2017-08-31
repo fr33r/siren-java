148f729605647d7833ad93bfbd52de3c424fa335
Jon Freer <freerjm@miamioh.edu>
2017-08-30 23:40:21 -0700

    Add more unit tests in ActionTester.

--- src/test/java/siren/ActionTester.java
+++ src/test/java/siren/ActionTester.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,10 +39,21 @@ public class ActionTester {
     public void href_nullHref_outcomeIs_IllegalArgumentException() {
 
         //arrange.
+        final String name = "testName";
         final URI href = null;
 
         //action.
-        this.actionBuilder.href(href).build();
+        this.actionBuilder.name(name).href(href).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void field_nullfield_outcomeIs_IllegalArgumentException() {
+
+        //arrange.
+        final Field field = null;
+
+        //action.
+        this.actionBuilder.field(field).build();
     }
 
     @Test
@@ -49,22 +61,27 @@ public class ActionTester {
 
         //arrange.
         final String name = "nameTest";
-        final String klass = "klassTest";
+        final String klass1 = "klassTest1";
+        final String klass2 = "klassTest2";
+        final List<String> klass = new ArrayList<String>();
+        klass.add(klass1);
+        klass.add(klass2);
         final String uri = "http://www.example.com/href";
         final URI href = new URI(uri);
         Action action =
                 this.actionBuilder
                         .name(name)
                         .href(href)
-                        .klass(klass)
+                        .klass(klass1)
+                        .klass(klass2)
                         .build();
 
         //action.
         List<String> actualClass = action.getKlass();
 
         //assert.
-        Assert.assertEquals(1, actualClass.size());
-        Assert.assertEquals(klass, actualClass.get(0));
+        Assert.assertEquals(2, actualClass.size());
+        Assert.assertEquals(klass, actualClass);
     }
 
     @Test
@@ -198,18 +215,29 @@ public class ActionTester {
 
         //arrange.
         final String name = "nameTest";
-        final Field<String> field =
+        final Field<String> field1 =
                 new Field.Builder<String>()
                         .name(name)
-                        .value("fieldTest")
+                        .value("fieldTest1")
                         .build();
+
+        final Field<String> field2 =
+                new Field.Builder<String>()
+                        .name(name)
+                        .value("fieldTest2")
+                        .build();
+        final List<Field<String>> fields =
+                new ArrayList<Field<String>>();
+        fields.add(field1);
+        fields.add(field2);
         final String type = "typeTest";
         final String uri = "http://www.example.com/href";
         final URI href = new URI(uri);
         Action action =
                 this.actionBuilder
                         .name(name)
-                        .field(field)
+                        .field(field1)
+                        .field(field2)
                         .href(href)
                         .type(type)
                         .build();
@@ -218,8 +246,8 @@ public class ActionTester {
         List<Field> actualFields = action.getFields();
 
         //assert.
-        Assert.assertEquals(1, actualFields.size());
-        Assert.assertEquals(field, actualFields.get(0));
+        Assert.assertEquals(2, actualFields.size());
+        Assert.assertEquals(fields, actualFields);
     }
 
     @Test
